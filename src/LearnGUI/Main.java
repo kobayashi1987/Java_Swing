1780b9fc455e44832a08ce71e40317288a49f19b
kobayashi1987 <sashagao1987@gmail.com>
2024-06-18 22:42:34 +0800

    20240618 2242
    Java Swing: Button

--- src/LearnGUI/Main.java
+++ src/LearnGUI/Main.java
@@ -1,95 +1,54 @@
 package LearnGUI;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//
+//        ImageIcon image = new ImageIcon("norway.png");
+//        JLabel label = new JLabel();
+//        label.setIcon(image);
+//        label.setText("Hi, I am a label");
+//        label.setVerticalAlignment(JLabel.TOP);
+//        label.setForeground(new Color(0x00FF00));
+//
+//        // study JButton
+//        MyFrame frame01 = new MyFrame();
+//        frame01.setTitle("Learn JButton");
+//        frame01.setSize(800, 600);
+//        frame01.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ensure the application exits when the frame is closed
+//        frame01.setLayout(new FlowLayout()); // Set a layout manager
+//        frame01.add(label);
+//        frame01.setVisible(true); // Make sure the frame is visible
+//    }
+//}
 
-public class Main {
+// study JButton
+
+
+public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        JFrame frame = new JFrame();  // creates an instance of frame
-        JLabel label = new JLabel("Hello World");  // creates an instance of label
-        JLabel label01 = new JLabel();  // creates an instance of label
-        JPanel redPanel = new JPanel(); // creates an instance of panel
-
-        // create a blue panel
-        JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.blue);
-        bluePanel.setBounds(250, 0, 250, 250);
-        bluePanel.setLayout(null);
-
-        // create a green panel
-        JPanel greenPanel = new JPanel();
-        greenPanel.setBackground(Color.green);
-        greenPanel.setBounds(0,250,500,250);
-//        greenPanel.setLayout(new BorderLayout());   // set layout manager
-        greenPanel.setLayout(null);
-
-
-        //set up the panel
-        redPanel.setBackground(Color.red);
-        redPanel.setBounds(0, 0, 250, 250);
-        redPanel.setLayout(null);   // set layout manager
-
-        // creating image icon
-        ImageIcon image = new ImageIcon("norway01.png"); // create an image icon
-        ImageIcon image01 = new ImageIcon("finland.png"); // create an image icon
-
-        // set up border
-        Border border = BorderFactory.createLineBorder(Color.green,3);
-
-
-
-
-        // set up the  label
+        // create label
+        ImageIcon image = new ImageIcon("norway.png");
+        JLabel label = new JLabel();
         label.setIcon(image);
-        label.setText("Bro, do you like code ?");
-        label.setHorizontalTextPosition(JLabel.CENTER);// set text left, right, or center
-        label.setVerticalTextPosition(JLabel.TOP);  // set text top, bottom, or center
-        label.setForeground(Color.black);
-        label.setFont(new Font("My Boli", Font.BOLD, 50));
-        label.setIconTextGap(40);
-        label.setBackground(Color.red);
-        label.setOpaque(true);
-        label.setBorder(border);
-        label.setVerticalAlignment(JLabel.CENTER); // set horizontal and vertical position
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setBounds(1000, 200, 800, 600);
+        label.setText("Hi, I am a label, gonna learn JButton next.");
+        label.setVerticalAlignment(JLabel.TOP);
+        label.setForeground(new Color(0x00FF00));
+        label.setBounds(0, 0, 800, 600);
 
-        //set up label01
-        label01.setText("Hello, jack ?");
-        label01.setIcon(image01);
-        label01.setVerticalAlignment(JLabel.BOTTOM);
-        label01.setHorizontalAlignment(JLabel.RIGHT);
-        label01.setBounds(0, 0, 200, 150);
-
-        // Add the label to the frame's content pane
-//        frame.getContentPane().add(label, BorderLayout.CENTER);
-
-        // setting up the frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
-        frame.setResizable(true);
-        frame.setSize(750, 750);
-        frame.setVisible(true);
-        frame.setTitle("Learn GUI");
-        frame.setIconImage(image.getImage()); // change icon of frame
-        frame.getContentPane().setBackground(new Color(0x000000));
-        frame.setLayout(null);
-
+        // create frame
+        MyFrame frame = new MyFrame();
+        frame.setTitle("Learn JButton");
+        frame.setSize(1200, 900);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ensure the application exits when the frame is closed
+//        frame.setLayout(new FlowLayout()); // Set a layout manager
         frame.add(label);
-        frame.add(label01);
-        bluePanel.add(label01);
-        frame.add(redPanel);
-        frame.add(bluePanel);
-        frame.add(greenPanel);
-
-        // Display the frame
-        frame.setVisible(true);
+        frame.setVisible(true); // Make sure the frame is visible
 
     }
 }
-
-//test github 20240618 2057
-
--- src/LearnGUI/MyFrame.java
+++ src/LearnGUI/MyFrame.java
@@ -2,20 +2,66 @@ package LearnGUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
+
+    // declare a button globally
+    JButton button;
+    JLabel label;
+
     MyFrame(){
 
+        //create an image icon
+        ImageIcon icon = new ImageIcon("finland.png");
+        ImageIcon icon1 = new ImageIcon("denmark.png");
 
-        this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE); //exit out of application
-        this.setResizable(false);
+        // create a label
+        label = new JLabel();
+        label.setIcon(icon1);
+        label.setBounds(800, 0, 800,600);
+        label.setText("Hi, I am Denmark, near to Norway.");
+        label.setForeground(new Color(0x00AA00));
+        label.setVisible(false);
+
+        // create a button
+        button = new JButton();
+        button.setBounds(300, 700, 250, 100);
+        button.addActionListener(this); // add action listener to button
+        button.setText("Click me");
+        button.setFocusable(false); // remove the focus of the button
+        button.setIcon(icon);
+        button.setHorizontalTextPosition(JButton.CENTER); // set the text position
+        button.setVerticalTextPosition(JButton.BOTTOM); // set the text position
+        button.setFont(new Font("Comic Sans", Font.BOLD, 25)); // set the font
+        button.setForeground(Color.cyan); // set the font color
+        button.setIconTextGap(10); // set the gap between text and icon
+        button.setBackground(Color.lightGray); // set the background color
+        button.setBorder(BorderFactory.createEtchedBorder()); // set the border
+
+        // set the frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
         this.setSize(800, 600);
         this.setVisible(true);
-        this.setTitle("Learn GUI");
+        this.setTitle("Learn JButton");
+        this.setLayout(null);
+        this.getContentPane().setBackground(new Color(0x123456)); // set the frame color
+        this.add(button);   // add button to the frame
+        this.add(label);    // add label to the frame
 
 
-        ImageIcon image = new ImageIcon("LearnGUI/resources/norway.png"); // create an image icon
-        this.setIconImage(image.getImage()); // change icon of frame
-        this.getContentPane().setBackground(new Color(0x000000));
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // when button is clicked, this will execute
+        if(e.getSource() == button){
+            System.out.println("Button clicked");
+            label.setVisible(true);
+            button.setEnabled(false); // disable the button
+        }
+    }
+
+
 }
