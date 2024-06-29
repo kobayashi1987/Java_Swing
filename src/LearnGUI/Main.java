03eeb381d6ad23a794d4132e0718e2a9e5868a3c
kobayashi1987 <sashagao1987@gmail.com>
2024-06-29 18:12:26 +0800

    20240618 2242
    Java Swing: launch a new window

--- src/LearnGUI/Main.java
+++ src/LearnGUI/Main.java
@@ -4,14 +4,39 @@ import javax.swing.*;
 import java.awt.*;
 
 
-// New Window
+// JOptionPane
+// JOptionpane is a class that is used to create a dialog box that prompts the user for a value or informs them of something.
+// JOptionPane.showMessageDialog(null, "Hello World"); // Display a message dialog box
 
 public class Main{
 
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        LaunchPage launchPage = new LaunchPage();
+        // Display a message dialog box
+//        JOptionPane.showMessageDialog(null, "Hello World","Title", JOptionPane.PLAIN_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello World","Title", JOptionPane.INFORMATION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello World","Title", JOptionPane.QUESTION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello World","Title", JOptionPane.WARNING_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello World","Title", JOptionPane.ERROR_MESSAGE);
 
+        // Display a confirm dialog box
+//        JOptionPane.showConfirmDialog(null, "Hello World","Title", JOptionPane.YES_NO_CANCEL_OPTION);
+//        int answer = JOptionPane.showConfirmDialog(null, "Hello World","Title", JOptionPane.YES_NO_CANCEL_OPTION);
+//        // input dialog box
+//        String name = JOptionPane.showInputDialog("What is your name?");
+//        System.out.println("Hello " + name);
+
+        // option dialog box
+        String[] responses = {"yes, I am", "no, i am not", "maybe yes, maybe no"};
+        ImageIcon icon = new ImageIcon("finland.png");
+        JOptionPane.showOptionDialog(null,
+                "you are awesome",
+                "secret message",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                icon,
+                responses,
+                0);
     }
 }
